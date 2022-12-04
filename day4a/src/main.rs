9715f0a65e1e7e0fa6e9a37dd14a933ef1555412
Loys Ollivier <loys.ollivier@arm.com>
2022-12-04 14:42:58 +0100

    Day4: map() and tuple

---
+++ day4a/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut tot = 0;
+    for line in include_str!("../input.txt").lines() {
+        let v: Vec<u8> = line.split(&[',', '-'][..]).map(|x| x.parse::<u8>().unwrap()).collect();
+        let (low1, high1, low2, high2) = (v[0], v[1], v[2], v[3]);
+        if low1 >= low2 && high1 <= high2 {
+            tot += 1;
+        } else if low2 >= low1 && high2 <= high1 {
+            tot += 1;
+        }
+    }
+    println!("Total: {}", tot);
+}
